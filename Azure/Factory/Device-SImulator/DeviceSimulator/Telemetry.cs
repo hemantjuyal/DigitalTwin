4868d25aa18525fadb0fe7ac2469e8a0636292a5
Manav-1106 <guduguntlamanav@gmail.com>
2023-10-13 11:39:33 +0530

    Initial Commit

---
+++ Azure/Factory/Device-SImulator/DeviceSimulator/Telemetry.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+/// <summary>
+/// Handles loading data from data file
+/// </summary>
+public class Telemetry
+{
+	private const string dataFile = "./data.csv";
+
+	public static List<TelemetryData> GetDataLines()
+    {
+		using (StreamReader sr = new StreamReader(dataFile))
+		{
+			List<TelemetryData> allTelemetryData = new List<TelemetryData>();
+			sr.ReadLine();
+			while (!sr.EndOfStream)
+			{
+				allTelemetryData.Add(CreateTelemetryData(sr.ReadLine()));
+			}
+			return allTelemetryData;
+		}
+    }
+
+	private static TelemetryData CreateTelemetryData(string line)
+    {
+		TelemetryData data = new TelemetryData();
+		string[] split = line.Split(',');
+		data.RoboticPalletizerID = split[0];
+		data.RoboticArmID = split[1];
+		bool.TryParse(split[2], out data.RoboticArmStatus);
+		double.TryParse(split[3], out data.RoboticArmPowerComsumption);
+		double.TryParse(split[4], out data.RoboticArmOperatingSpeed);
+		double.TryParse(split[5], out data.RoboticArmLoadCapacity);
+		double.TryParse(split[6], out data.ConveyerBeltSpeed);
+		double.TryParse(split[7], out data.LightCurtainResolution);
+		double.TryParse(split[8], out data.LightCurtainRange);
+		double.TryParse(split[9], out data.PalletTurnTableRotationSpeed);
+		data.DoorLastAccessedTime = split[10];
+		bool.TryParse(split[11], out data.DoorStatus);
+		double.TryParse(split[12], out data.PalletStretchMachineWrappingSpeed);
+		bool.TryParse(split[13], out data.PalletStretchMachineWrappingFilmRollStatus);
+		double.TryParse(split[14], out data.PalletStretchMachineWrappingFilmRollUsage);
+		return data;
+	}
+}
---
+++ Azure/Factory/Device-SImulator/DeviceSimulator/TelemetryData.cs
@@ -0,0 +1,42 @@
+﻿/// <summary>
+/// Holds data for telemetry
+/// </summary>
+public class TelemetryData
+{
+	public string RoboticPalletizerID;
+	public string RoboticArmID;
+	public bool RoboticArmStatus;
+	public double RoboticArmPowerComsumption;
+	public double RoboticArmOperatingSpeed;
+	public double RoboticArmLoadCapacity;
+	public double ConveyerBeltSpeed;
+	public double LightCurtainResolution;
+	public double LightCurtainRange;
+	public double PalletTurnTableRotationSpeed;
+	public string DoorLastAccessedTime;
+	public bool DoorStatus;
+	public double PalletStretchMachineWrappingSpeed;
+	public bool PalletStretchMachineWrappingFilmRollStatus;
+	public double PalletStretchMachineWrappingFilmRollUsage;
+
+	public TelemetryData() { }
+    
+	public TelemetryData(TelemetryData data)
+    {
+		RoboticPalletizerID = data.RoboticPalletizerID;
+		RoboticArmID = data.RoboticArmID;
+		RoboticArmStatus = data.RoboticArmStatus;
+		RoboticArmPowerComsumption = data.RoboticArmPowerComsumption;
+		RoboticArmOperatingSpeed = data.RoboticArmOperatingSpeed;
+		RoboticArmLoadCapacity = data.RoboticArmLoadCapacity;
+		ConveyerBeltSpeed = data.ConveyerBeltSpeed;
+		LightCurtainResolution = data.LightCurtainResolution;
+		LightCurtainRange = data.LightCurtainRange;
+		PalletTurnTableRotationSpeed = data.PalletTurnTableRotationSpeed;
+		DoorLastAccessedTime = ata.DoorLastAccessedTime;
+		DoorStatus = data.DoorStatus;
+		PalletStretchMachineWrappingSpeed = data.PalletStretchMachineWrappingSpeed;
+		PalletStretchMachineWrappingFilmRollStatus = data.PalletStretchMachineWrappingFilmRollStatus;
+		PalletStretchMachineWrappingFilmRollUsage = data.PalletStretchMachineWrappingFilmRollUsage;
+    }
+}
